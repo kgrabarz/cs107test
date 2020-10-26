56667db7b295bdc128978e47daac9a563b2a4e3f
Kristen Grabarz <grabarz.kristen@gmail.com>
2020-10-26 19:33:52 -0400

    updated roots and test roots

--- test_roots.py
+++ test_roots.py
@@ -1,6 +1,24 @@
-# This is a temp pytest file while I wait for lecture 13 in class
-def inc(x):
-    return x + 1
+import pytest
+import roots
 
-def test_answer():
-    assert inc(3) == 4
+def test_quadroots_result():
+    assert roots.quad_roots(1.0, 1.0, -12.0) == ((3+0j), (-4+0j))
+
+def test_quadroots_types():
+    with pytest.raises(TypeError):
+        roots.quad_roots("", "green", "hi")
+
+def test_quadroots_zerocoeff():
+    with pytest.raises(ValueError):
+        roots.quad_roots(a=0.0)
+
+def test_linearoots_result():
+    assert roots.linear_roots(2.0, -3.0) == 1.5
+
+def test_linearroots_types():
+    with pytest.raises(TypeError):
+        roots.linear_roots("ocean", 6.0)
+
+def test_linearroots_zerocoeff():
+    with pytest.raises(ValueError):
+        roots.linear_roots(a=0.0)
